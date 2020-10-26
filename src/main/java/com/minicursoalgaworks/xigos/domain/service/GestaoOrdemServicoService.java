a89baa99ac105ffb03248d6ed99146b87bfa14f0
enakata <edsonnakata2@gmail.com>
2020-10-26 16:37:10 -0400

    Verificação da existencia do cliente

--- src/main/java/com/minicursoalgaworks/xigos/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/minicursoalgaworks/xigos/domain/service/GestaoOrdemServicoService.java
@@ -5,8 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.minicursoalgaworks.xigos.domain.exception.NegocioException;
+import com.minicursoalgaworks.xigos.domain.model.Cliente;
 import com.minicursoalgaworks.xigos.domain.model.OrdemServico;
 import com.minicursoalgaworks.xigos.domain.model.StatusOrdemServico;
+import com.minicursoalgaworks.xigos.domain.repository.ClienteRepository;
 import com.minicursoalgaworks.xigos.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -15,7 +18,14 @@ public class GestaoOrdemServicoService {
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public OrdemServico criar(OrdemServico ordemServico) {
+		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
+				.orElseThrow(()-> new NegocioException("Cliente não encontrado."));
+		
+		ordemServico.setCliente(cliente);
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		ordemServico.setDataAbertura(LocalDateTime.now());
 		
