b721a3c0a8a699af9b83ac39c8a6d42ae486e370
enakata <edsonnakata2@gmail.com>
2020-10-26 16:12:33 -0400

    Criação da função criar no service de ordem de serviço

--- src/main/java/com/minicursoalgaworks/xigos/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/minicursoalgaworks/xigos/domain/service/GestaoOrdemServicoService.java
@@ -1,8 +1,26 @@
 package com.minicursoalgaworks.xigos.domain.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.minicursoalgaworks.xigos.domain.model.OrdemServico;
+import com.minicursoalgaworks.xigos.domain.model.StatusOrdemServico;
+import com.minicursoalgaworks.xigos.domain.repository.OrdemServicoRepository;
 
 @Service
 public class GestaoOrdemServicoService {
-
+	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	
+	public OrdemServico criar(OrdemServico ordemServico) {
+		ordemServico.setStatus(StatusOrdemServico.ABERTA);
+		ordemServico.setDataAbertura(LocalDateTime.now());
+		
+		return ordemServicoRepository.save(ordemServico);
+	}
+	
+	
 }
