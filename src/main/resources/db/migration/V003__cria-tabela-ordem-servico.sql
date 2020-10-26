bbbefdcdcddbac5f78863fd4bce66fc6947e020c
enakata <edsonnakata2@gmail.com>
2020-10-26 16:29:26 -0400

    Criação do endpoint de criar ordem de serviço e refatoração da migration

--- src/main/resources/db/migration/V003__cria-tabela-ordem-servico.sql
+++ src/main/resources/db/migration/V003__cria-tabela-ordem-servico.sql
@@ -1,11 +1,10 @@
 CREATE TABLE ordem_servico (
-	 id int NOT NULL,
+	 id serial PRIMARY KEY,
 	 cliente_id int NOT NULL,
 	 descricao text NOT null,
 	 preco decimal(10,2) not null,
 	 status varchar(20) not null,
 	 data_abertura timestamp not null,
 	 data_finalizacao timestamp,
-	 PRIMARY KEY (id),
 	 FOREIGN KEY (cliente_id) REFERENCES cliente (id)
 );
